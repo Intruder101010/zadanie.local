fb429a5786af9af818fb8733f34637355372184f
Intuder101010 <muzikant18@gmail.com>
2018-06-02 20:55:35 +0300

    Через генератор кода Gii была добавлена Admin консоль с возможностью изменять, удалять и добавлять посты.

---
+++ modules/admin/models/Post.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\modules\admin\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property int $id
+ * @property string $mail
+ * @property string $name
+ * @property string $text
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['mail', 'name', 'text'], 'required'],
+            [['text'], 'string'],
+            [['mail', 'name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'mail' => 'Mail',
+            'name' => 'Name',
+            'text' => 'Text',
+        ];
+    }
+}
